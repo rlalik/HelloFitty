7773f51fffcdffcd908fdc8bf921b289fdd4eedc
Rafal Lalik <Rafal.Lalik@ph.tum.de>
2015-06-08 19:04:55 +0200

    Initial commit

---
+++ examples/example.cxx
@@ -0,0 +1,142 @@
+#include <FitterFactory.h>
+
+#include <fstream>
+
+int main()
+{
+	const char * parname = "testpars.txt";
+	// make input par file
+	std::fstream parfile(parname, fstream::in);
+	if (!parfile.is_open())
+	{
+		std::cout << "Creating parameter file" << std::endl;
+		std::fstream parfile2(parname, fstream::out);
+		if (parfile2.is_open())
+		{
+			parfile2 << " test_hist gaus(0) expo(3)  0 0 10 10 1 1 1 -1" << std::endl;
+			parfile2.close();
+		}
+		else
+		{
+			std::cerr << "Parameter file can't be created, unable to execute example." << std::endl;
+			std::exit(EXIT_FAILURE);
+		}
+	}
+	else
+	{
+		std::cout << "Good, parameter file exists." << std::endl;
+	}
+
+	TH1I *unnamed = new TH1I("test_hist","",100,0,10);
+	unnamed->SetBinContent(1,7290);
+	unnamed->SetBinContent(2,6750);
+	unnamed->SetBinContent(3,6383);
+	unnamed->SetBinContent(4,6255);
+	unnamed->SetBinContent(5,6037);
+	unnamed->SetBinContent(6,5577);
+	unnamed->SetBinContent(7,5327);
+	unnamed->SetBinContent(8,5070);
+	unnamed->SetBinContent(9,4779);
+	unnamed->SetBinContent(10,4695);
+	unnamed->SetBinContent(11,4444);
+	unnamed->SetBinContent(12,4276);
+	unnamed->SetBinContent(13,3976);
+	unnamed->SetBinContent(14,3939);
+	unnamed->SetBinContent(15,3582);
+	unnamed->SetBinContent(16,3434);
+	unnamed->SetBinContent(17,3419);
+	unnamed->SetBinContent(18,3366);
+	unnamed->SetBinContent(19,3302);
+	unnamed->SetBinContent(20,3376);
+	unnamed->SetBinContent(21,3581);
+	unnamed->SetBinContent(22,3623);
+	unnamed->SetBinContent(23,3999);
+	unnamed->SetBinContent(24,4424);
+	unnamed->SetBinContent(25,4999);
+	unnamed->SetBinContent(26,5350);
+	unnamed->SetBinContent(27,5976);
+	unnamed->SetBinContent(28,6264);
+	unnamed->SetBinContent(29,6523);
+	unnamed->SetBinContent(30,6633);
+	unnamed->SetBinContent(31,6544);
+	unnamed->SetBinContent(32,6289);
+	unnamed->SetBinContent(33,5859);
+	unnamed->SetBinContent(34,5367);
+	unnamed->SetBinContent(35,4723);
+	unnamed->SetBinContent(36,4021);
+	unnamed->SetBinContent(37,3350);
+	unnamed->SetBinContent(38,2801);
+	unnamed->SetBinContent(39,2279);
+	unnamed->SetBinContent(40,1833);
+	unnamed->SetBinContent(41,1502);
+	unnamed->SetBinContent(42,1245);
+	unnamed->SetBinContent(43,1069);
+	unnamed->SetBinContent(44,958);
+	unnamed->SetBinContent(45,953);
+	unnamed->SetBinContent(46,747);
+	unnamed->SetBinContent(47,726);
+	unnamed->SetBinContent(48,704);
+	unnamed->SetBinContent(49,656);
+	unnamed->SetBinContent(50,594);
+	unnamed->SetBinContent(51,585);
+	unnamed->SetBinContent(52,586);
+	unnamed->SetBinContent(53,518);
+	unnamed->SetBinContent(54,468);
+	unnamed->SetBinContent(55,490);
+	unnamed->SetBinContent(56,476);
+	unnamed->SetBinContent(57,423);
+	unnamed->SetBinContent(58,436);
+	unnamed->SetBinContent(59,407);
+	unnamed->SetBinContent(60,364);
+	unnamed->SetBinContent(61,380);
+	unnamed->SetBinContent(62,345);
+	unnamed->SetBinContent(63,322);
+	unnamed->SetBinContent(64,321);
+	unnamed->SetBinContent(65,319);
+	unnamed->SetBinContent(66,277);
+	unnamed->SetBinContent(67,264);
+	unnamed->SetBinContent(68,228);
+	unnamed->SetBinContent(69,238);
+	unnamed->SetBinContent(70,218);
+	unnamed->SetBinContent(71,236);
+	unnamed->SetBinContent(72,221);
+	unnamed->SetBinContent(73,201);
+	unnamed->SetBinContent(74,196);
+	unnamed->SetBinContent(75,210);
+	unnamed->SetBinContent(76,153);
+	unnamed->SetBinContent(77,146);
+	unnamed->SetBinContent(78,154);
+	unnamed->SetBinContent(79,130);
+	unnamed->SetBinContent(80,131);
+	unnamed->SetBinContent(81,135);
+	unnamed->SetBinContent(82,115);
+	unnamed->SetBinContent(83,117);
+	unnamed->SetBinContent(84,126);
+	unnamed->SetBinContent(85,100);
+	unnamed->SetBinContent(86,107);
+	unnamed->SetBinContent(87,78);
+	unnamed->SetBinContent(88,73);
+	unnamed->SetBinContent(89,93);
+	unnamed->SetBinContent(90,72);
+	unnamed->SetBinContent(91,78);
+	unnamed->SetBinContent(92,73);
+	unnamed->SetBinContent(93,90);
+	unnamed->SetBinContent(94,65);
+	unnamed->SetBinContent(95,55);
+	unnamed->SetBinContent(96,64);
+	unnamed->SetBinContent(97,63);
+	unnamed->SetBinContent(98,67);
+	unnamed->SetBinContent(99,62);
+	unnamed->SetBinContent(100,55);
+	unnamed->SetEntries(210000);
+
+	FitterFactory ff;
+	ff.initFactoryFromFile("testpars.txt", "testpars.out");
+
+	HistFitParams hfp;
+	ff.findParams("test_hist", hfp);
+	ff.fit(hfp, unnamed);
+	ff.exportFactoryToFile();
+
+	return 0;
+}
---
+++ inc/FitterFactory.h
@@ -0,0 +1,107 @@
+/*
+    <one line to give the program's name and a brief idea of what it does.>
+    Copyright (C) 2012  <copyright holder> <email>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+#ifndef FITTERFACTORY_H
+#define FITTERFACTORY_H
+
+#include <map>
+
+#include <TF1.h>
+#include <TH1.h>
+
+struct ParamValues {
+	Double_t val;		// value
+	Double_t l;			// lower limit
+	Double_t u;			// upper limit
+
+	ParamValues() : val(0), l(0), u(0) {}
+};
+
+class HistFitParams
+{
+public:
+	TString histname;	// keeps hist name
+	TString funname;	// keeps func name
+	TString f_sig, f_bg;
+// 	TString func;		// fit function
+// 	TString func;		// fit function
+	Int_t allparnum;	// num of pars
+	Int_t rebin;		// rebin, 0 == no rebin
+	Float_t fun_l;		// function range
+	Float_t fun_u;		// function range
+	Bool_t fit_disabled;
+
+	ParamValues * pars;
+	TF1 * funSig;
+	TF1 * funBg;
+	TF1 * funSum;
+
+	HistFitParams();
+	HistFitParams(const HistFitParams & hfp);
+	~HistFitParams();
+	HistFitParams & operator=(const HistFitParams & hfp);
+	void Init(const TString & h, const TString & fsig, const TString & fbg, Int_t bgn, Double_t f_l, Double_t f_u);
+	void SetParam(Int_t par, Double_t val);
+	void SetParam(Int_t par, Double_t val, Double_t l, Double_t u);
+	void Print() const;
+	void PrintInline() const;
+
+	TString exportEntry() const;
+
+	static const HistFitParams parseEntryFromFile(const TString & line);
+
+// 	TF1 *fitV;
+};
+
+class FitterFactory
+{
+public:
+	enum FLAGS { ALWAYS_REF, ALWAYS_AUX, ALWAYS_NEWER };
+	enum FIND_FLAGS { NOT_FOUND, USE_FOUND, USE_DEFAULT };
+
+	FitterFactory(FLAGS flags = ALWAYS_NEWER) : flags(flags), has_defaults(false), par_ref(nullptr), par_aux(nullptr) {}
+
+	FLAGS setFlags(FLAGS new_flags);
+	void setDefaultParameters(HistFitParams defs);
+
+	bool initFactoryFromFile(const char * filename, const char * auxname = 0);
+	bool exportFactoryToFile();
+
+	FIND_FLAGS findParams(TH1 * hist, HistFitParams & hfp, bool use_defaults = true) const;
+	FIND_FLAGS findParams(const char * name, HistFitParams & hfp, bool use_defaults = true) const;
+
+	bool fit(TH1 * hist, const char * pars = "B,Q", const char * gpars = "");
+	static bool fit(HistFitParams & hfp, TH1 * hist, const char * pars = "B,Q", const char * gpars = "");
+
+private:
+	bool import_parameters(const char * filename);
+	bool export_parameters(const char * filename);
+
+	FitterFactory::FLAGS flags;
+
+	bool has_defaults;
+
+	const char * par_ref;
+	const char * par_aux;
+
+	HistFitParams defpars;
+	std::map<TString, HistFitParams> hfpmap;
+};
+
+#endif // FITTERFACTORY_H
